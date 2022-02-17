a728e72c0dde6962e64feaf4d89af5c93b8136f2
Nick Zemskoy <nzemskoy@gmail.com>
2022-02-17 14:49:08 +0300

    Added mapper Snack_Case

--- src/main/java/ru/zenicko/restbackend/RestBackendApplication.java
+++ src/main/java/ru/zenicko/restbackend/RestBackendApplication.java
@@ -1,13 +1,21 @@
 package ru.zenicko.restbackend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestBackendApplication.class, args);
+    }
+
+    @Bean
+    public ObjectMapper jacksonMapper() {
+        return new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+    }
 
 }
--- src/main/java/ru/zenicko/restbackend/domain/LoginInfo.java
+++ src/main/java/ru/zenicko/restbackend/domain/LoginInfo.java
@@ -1,10 +1,14 @@
 package ru.zenicko.restbackend.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginInfo {
     private String userName;
     private String password;
