4baca83f14ca894679e2701ff3c4a903048a24a3
Nick Zemskoy <nzemskoy@gmail.com>
2022-02-17 16:19:30 +0300

    Added swagger 2.0

--- src/main/java/ru/zenicko/restbackend/RestBackendApplication.java
+++ src/main/java/ru/zenicko/restbackend/RestBackendApplication.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class RestBackendApplication {
@@ -16,6 +20,15 @@ public class RestBackendApplication {
     @Bean
     public ObjectMapper jacksonMapper() {
         return new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
--- src/main/java/ru/zenicko/restbackend/controller/BankController.java
+++ src/main/java/ru/zenicko/restbackend/controller/BankController.java
@@ -1,5 +1,6 @@
 package ru.zenicko.restbackend.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
